75662301ab1a5b3800a555b9e729a4c94368dede
ZucchiniSky <scottbommarito@gmail.com>
2015-02-20 12:55:09 -0500

    lol welp

--- vectorspace.py
+++ vectorspace.py
@@ -94,7 +94,7 @@ def retrieveDocuments(query, index, docw, queryw):
         weight, query = weighTerm(token, index, tokens.count(token), queryw, queryFreq, query)
     cosinequery = 0.0
     for token in query:
-        cosinequery += query[token] * query[token]
+        cosinequery += query[1][token] * query[1][token]
     cosinequery = math.sqrt(cosinequery)
     rank = []
     for doc in docs:
@@ -102,7 +102,7 @@ def retrieveDocuments(query, index, docw, queryw):
         for token in set(tokens):
             if index[2][doc].get(token) is None:
                 continue
-            weight += query[token] * index[2][doc][token]
+            weight += query[1][token] * index[2][doc][token]
         cosinedoc = 0.0
         for token in index[2][doc]:
             cosinedoc += index[2][doc][token] * index[2][doc][token]
